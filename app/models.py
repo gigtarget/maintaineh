62c7dd0789a3d5ce3696f663f6392a29ad9f6991
gigtarget <jatingots9@gmail.com>
2025-06-08 00:08:27 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,11 +1,14 @@
-from app import db, login_manager
-from flask_login import UserMixin
+from app import db
+from datetime import datetime
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-class User(db.Model, UserMixin):
+class QRBatch(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    qrcodes = db.relationship('QRCode', backref='batch', lazy=True)
+
+class QRCode(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    batch_id = db.Column(db.Integer, db.ForeignKey('q_r_batch.id'), nullable=False)
+    qr_type = db.Column(db.String(20))  # master, service, sub1...sub8
+    image_url = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
