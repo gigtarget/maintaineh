ab3991264f831c747578a5e81cea0c2397ebdffb
gigtarget <jatingots9@gmail.com>
2025-06-08 00:01:54 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from dotenv import load_dotenv
+from flask_session import Session
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -12,9 +13,12 @@ login_manager.login_view = 'routes.login'
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'your-secret-key')
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'your-secret')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    app.config['SESSION_TYPE'] = 'filesystem'
+    Session(app)
 
     db.init_app(app)
     login_manager.init_app(app)
@@ -22,8 +26,8 @@ def create_app():
     from app.routes import routes
     app.register_blueprint(routes)
 
+    from app import models
     with app.app_context():
-        from app import models
         db.create_all()
 
     return app
