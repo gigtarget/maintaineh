ba69f0adc477c7bffd17044d7fa9ead9bd0367cb
gigtarget <jatingots9@gmail.com>
2025-06-26 23:28:26 -0400

    Update decorators.py

--- app/decorators.py
+++ app/decorators.py
@@ -1,11 +1,17 @@
 from functools import wraps
 from flask import session, redirect, url_for, flash
+from flask_login import current_user
 
-def subuser_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if not session.get('subuser_id'):
-            flash("Please log in as sub-user", "danger")
-            return redirect(url_for("routes.subuser_login"))
-        return f(*args, **kwargs)
-    return decorated_function
+def user_or_subuser_required(view_func):
+    @wraps(view_func)
+    def wrapper(*args, **kwargs):
+        # Main user (Flask-Login)
+        if current_user.is_authenticated:
+            return view_func(*args, **kwargs)
+        # Subuser (session-based)
+        elif session.get('subuser_id'):
+            return view_func(*args, **kwargs)
+        else:
+            flash("Please log in to continue.", "danger")
+            return redirect(url_for("routes.user_login"))
+    return wrapper
