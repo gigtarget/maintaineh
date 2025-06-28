5f78683c9cdfd20f4772fe27a970715d2eb54dca
gigtarget <jatingots9@gmail.com>
2025-06-27 23:34:16 -0400

    Update decorators.py

--- app/decorators.py
+++ app/decorators.py
@@ -5,6 +5,9 @@ from flask_login import current_user
 def subuser_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
+        print("\n======== DEBUG (subuser_required) ========")
+        print("session:", dict(session))
+        print("==========================================")
         if not session.get('subuser_id'):
             flash("Please log in as sub-user", "danger")
             return redirect(url_for("routes.subuser_login"))
@@ -14,8 +17,11 @@ def subuser_required(f):
 def user_or_subuser_required(view_func):
     @wraps(view_func)
     def wrapper(*args, **kwargs):
-        print("DEBUG: current_user.is_authenticated:", current_user.is_authenticated)
-        print("DEBUG: session.get('subuser_id'):", session.get('subuser_id'))
+        print("\n======== DEBUG (user_or_subuser_required) ========")
+        print("current_user.is_authenticated:", current_user.is_authenticated)
+        print("session:", dict(session))
+        print("session.get('subuser_id'):", session.get('subuser_id'))
+        print("==================================================")
         if current_user.is_authenticated:
             return view_func(*args, **kwargs)
         elif session.get('subuser_id'):
@@ -24,4 +30,3 @@ def user_or_subuser_required(view_func):
             flash("Please log in to continue.", "danger")
             return redirect(url_for("routes.user_login"))
     return wrapper
-
